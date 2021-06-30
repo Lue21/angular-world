82185ca257f395c3ce81a4fc9f8c69f3b410de34
Kate Pravosud <pravosud@gmail.com>
2021-06-30 18:18:53 +0300

    feat: implement routing, make MainLayout

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {FirstPageComponent} from './first-page/first-page.component';
+import {SecondPageComponent} from './second-page/second-page.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: FirstPageComponent},
+      {path: 'second', component: SecondPageComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {FirstPageComponent} from './first-page/first-page.component';
+import {SecondPageComponent} from './second-page/second-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    FirstPageComponent,
+    SecondPageComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
